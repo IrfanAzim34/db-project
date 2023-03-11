586ac92cb062557299243f126f5bea530d79c5c3
Irfan Azim <irfanteipov@gmail.com>
2023-03-11 04:41:39 +0300

    Added Files

--- Queries/HR2.sql
+++ Queries/HR2.sql
@@ -11,4 +11,14 @@ select EMAIL,SALARY from EMPLOYEES where SALARY < 4000;
 select * from EMPLOYEES where JOB_ID = 'IT_PROG' or JOB_ID = 'SA_REP';
 
 --get me first_name,last_name salary who is making more than 5000 and less than 10000
+--if we use 'between' key it will be include 5000 and 10000
 select FIRST_NAME,LAST_NAME,SALARY from EMPLOYEES where SALARY between 5000 and 10000;
+
+--get me all info where emp_id 130 to 170
+select * from EMPLOYEES where EMPLOYEE_ID between 130 and  170;
+
+--get me all info where emp_id 135 176 154 129
+select * from EMPLOYEES where EMPLOYEE_ID in (135,176,154,129);
+
+--get me city of where country_id IT,US,UK
+select CITY,COUNTRY_ID from LOCATIONS where COUNTRY_ID in ('IT','US','UK');
