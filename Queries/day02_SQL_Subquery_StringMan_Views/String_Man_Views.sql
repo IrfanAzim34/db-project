34c99a97332a1eed55062d8d67c9943f76efa277
Irfan Azim <irfanteipov@gmail.com>
2023-03-13 19:15:43 +0300

    Added Files

--- Queries/day02_SQL_Subquery_StringMan_Views/String_Man_Views.sql
+++ Queries/day02_SQL_Subquery_StringMan_Views/String_Man_Views.sql
@@ -35,3 +35,10 @@ select "full_name" from EMAILLIST;
 
 --to remove view
 drop view EMAILLIST;
+
+--find the highest 57th salary
+select min(SALARY) from (select distinct SALARY from EMPLOYEES order by SALARY desc) where ROWNUM < 58;
+
+--find employee info who is making 58th highest salary
+select * from EMPLOYEES where SALARY =
+(select min(SALARY) from (select distinct SALARY from EMPLOYEES order by SALARY desc) where ROWNUM < 58);
--- Queries/day02_SQL_Subquery_StringMan_Views/h1.sql
+++
@@ -1,6 +0,0 @@
---find the highest 57th salary
-select min(SALARY) from (select distinct SALARY from EMPLOYEES order by SALARY desc) where ROWNUM < 58;
-
---find employee info who is making 58th highest salary
-select * from EMPLOYEES where SALARY =
-(select min(SALARY) from (select distinct SALARY from EMPLOYEES order by SALARY desc) where ROWNUM < 58);
